22519e702ed837429fac08bb56d4d336dae6a5e2
pisue <pisue22@naver.com>
2024-09-27 02:52:08 +0900

    Feat: PointService 동시성 제어

--- src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -1,10 +1,9 @@
 package io.hhplus.tdd.point.service;
 
-import io.hhplus.tdd.database.PointHistoryTable;
-import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.entity.PointHistory;
 import io.hhplus.tdd.point.entity.TransactionType;
 import io.hhplus.tdd.point.entity.UserPoint;
+import io.hhplus.tdd.point.exception.PointException;
 import io.hhplus.tdd.point.service.manager.PointHistoryManager;
 import io.hhplus.tdd.point.service.manager.PointManager;
 import io.hhplus.tdd.point.service.reader.PointHistoryReader;
@@ -18,7 +17,8 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -84,8 +84,28 @@ class PointServiceTest {
     }
 
     @Test
+    @DisplayName("사용자의 포인트 내역이 존재하지 않을 때")
+    void 포인트_내역이_존재하지_않을_때() {
+        //Given
+        long userId = 1L;
+        List<PointHistory> expectedPointHistories = List.of();
+        when(pointService.readHistory(userId)).thenReturn(expectedPointHistories);
+        when(pointHistoryReader.read(userId)).thenThrow(new PointException("포인트 내역이 존재하지 않습니다."));
+
+        //when
+        PointException exception = assertThrows(PointException.class, () -> pointService.readHistory(userId));
+
+
+        //then
+        verify(pointHistoryReader, times(1)).read(userId);
+
+        assertEquals("포인트 내역이 존재하지 않습니다.", exception.getMessage());
+
+    }
+
+    @Test
     @DisplayName("사용자의 포인트를 충전합니다")
-    void charge_shouldUpdateUserPoint() {
+    void 포인트_충전() {
         //Given
         long userId = 1L;
         long amount = 500L;
@@ -128,5 +148,60 @@ class PointServiceTest {
                 .append(eq(userId), eq(amount), eq(TransactionType.USE), anyLong());
         assertEquals(expectedUserPoint, result);
     }
+
+    @Test
+    @DisplayName("보유 포인트보다 사용할 포인트가 많을 때 예외")
+    void 보유_포인트보다_사용할_포인트가_많을_때() {
+        //given
+        long userId = 1L;
+        long useAmount = 500L;
+        when(pointManager.use(userId, useAmount)).thenThrow(new PointException("사용할 포인트가 부족합니다."));
+        when(pointService.read(userId)).thenReturn(new UserPoint(userId, 1000L, System.currentTimeMillis()));
+        //when
+        PointException exception = assertThrows(PointException.class, () -> pointService.use(userId, useAmount));
+
+        verify(pointManager, times(1)).use(userId, useAmount);
+        verify(pointHistoryManager, never()).append(anyLong(), anyLong(), any(), anyLong());
+
+        //then
+        assertEquals("사용할 포인트가 부족합니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("사용할 포인트가 음수일 때")
+    void 사용할_포인트가_음수일_때() {
+        //given
+        long userId = 1L;
+        long useAmount = -500L;
+        when(pointManager.use(userId, useAmount)).thenThrow(new PointException("적립할 포인트는 0보다 커야 합니다."));
+        when(pointService.read(userId)).thenReturn(new UserPoint(userId, 1000L, System.currentTimeMillis()));
+        //when
+        PointException exception = assertThrows(PointException.class, () -> pointService.use(userId, useAmount));
+
+        verify(pointManager, times(1)).use(userId, useAmount);
+        verify(pointHistoryManager, never()).append(anyLong(), anyLong(), any(), anyLong());
+
+        //then
+        assertEquals("적립할 포인트는 0보다 커야 합니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("최대 잔고를 넘겼을 때")
+    void 최대_잔고를_넘겼을_때() {
+        //given
+        long userId = 1L;
+        long chargeAmount = 100000L;
+        when(pointService.read(userId)).thenReturn(new UserPoint(userId, 1000L, System.currentTimeMillis()));
+        when(pointManager.charge(userId, chargeAmount)).thenThrow(new PointException("최대 잔고는 99999를 넘을 수 없습니다."));
+        //when
+        PointException exception = assertThrows(PointException.class, () -> pointService.charge(userId, chargeAmount));
+
+        verify(pointManager, times(1)).charge(userId, chargeAmount);
+        verify(pointHistoryManager, never()).append(anyLong(), anyLong(), any(), anyLong());
+
+        //then
+        assertEquals("최대 잔고는 99999를 넘을 수 없습니다.", exception.getMessage());
+    }
+
 
 }
