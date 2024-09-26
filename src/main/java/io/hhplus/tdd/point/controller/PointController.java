347c6a023988878806d0ca8abdd7e5d544b7f153
pisue <pisue22@naver.com>
2024-09-26 22:18:44 +0900

    Chore: 초기 패키징 구조 생성

--- src/main/java/io/hhplus/tdd/point/controller/PointController.java
+++ src/main/java/io/hhplus/tdd/point/controller/PointController.java
@@ -1,5 +1,7 @@
-package io.hhplus.tdd.point;
+package io.hhplus.tdd.point.controller;
 
+import io.hhplus.tdd.point.entity.PointHistory;
+import io.hhplus.tdd.point.entity.UserPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
